2d4f91ef69ca1a2ad8073be0e9cd14417a8c4b60
PhanHongAnh <honganh.p256@gmail.com>
2018-07-13 23:38:44 +0700

    Chapter 10 Updating, showing, and deleting users

--- Gemfile
+++ Gemfile
@@ -5,11 +5,14 @@ ruby "2.5.1"
 
 gem "bcrypt", "3.1.12"
 gem "bootstrap-sass", "3.3.7"
+gem "bootstrap-will_paginate", "~> 1.0"
+gem "faker", "~> 1.6", ">= 1.6.6"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.0"
 gem "sass-rails", "~> 5.0"
 gem "sqlite3"
 gem "uglifier", ">= 1.3.0"
+gem "will_paginate", "~> 3.1", ">= 3.1.6"
 
 gem "bootsnap", ">= 1.1.0", require: false
 gem "coffee-rails", "~> 4.2"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page],
+      per_page: Settings.paginate.per_page).order("created_at desc")
+  end
+
   def show
-    @user = User.find_by_id params[:id]
     return if @user
     redirect_to root_path
   end
@@ -13,11 +22,31 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t "welcome"
+      flash[:success] = t ".flash1"
       redirect_to @user
     else
       render :new
     end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".flash2"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".flash3"
+    else
+      flash[:danger] = t ".flash4"
+    end
+    redirect_to users_url
   end
 
   private
@@ -25,5 +54,26 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    flash[:danger] = t "users.flash0" if @user.nil?
+  end
+
+  def logged_in_user
+    return false if logged_in?
+    store_location
+    flash[:danger] = t "users.flash5"
+    redirect_to login_url
+  end
+
+  def correct_user
+    find_user
+    redirect_to root_path unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
